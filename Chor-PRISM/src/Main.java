1e95d257e4df907f94069f0c29117feaace0dfc4
adeleveschetti <adele.veschetti@gmail.com>
2023-04-25 14:12:38 +0200

    small changes

--- Chor-PRISM/src/Main.java
+++ Chor-PRISM/src/Main.java
@@ -17,7 +17,7 @@ import parser.LanguageParser;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		String fileName = "example.language";
+		String fileName = "./generated-sources/example.language";
 		FileInputStream is = new FileInputStream(fileName);
 		@SuppressWarnings("deprecation")
 		ANTLRInputStream input = new ANTLRInputStream(is);
@@ -28,22 +28,14 @@ public class Main {
 		LanguageVisitorImpl visitor = new LanguageVisitorImpl();
 		Node ast = visitor.visit(t);
 		String code = ast.generateCode("",-1,-1,null,0);
-		System.out.println(code);
-		/*
-		HashMap<String,ArrayList<Integer>> mapStatesBranches = null ;
-		HashMap<String,ArrayList<Integer>> mapStates = null ;
-		ArrayList<String> roles = null;
-		ArrayList<String> allRoles = null;
-		toRet = ast.codeGenerator(toRet,mapStates,mapStatesBranches,roles,allRoles,0,0);
-		
-		File file = new File("GeneratedCode.prism");
+		File file = new File("./generated-sources/GeneratedCode.prism");
 		if (!file.exists()) {
             file.createNewFile();
         }
 		BufferedWriter output = new BufferedWriter(new FileWriter(file));
-	    output.write(toRet);
+	    output.write(code);
 	    output.close();
-	    */
+
 	}
 	
 }
